41ab6174c3596e2e97a1112d2029b7f7663d2170
aquaworm888 <128005376+aquaworm888@users.noreply.github.com>
2025-06-12 00:08:43 +0300

    Add files via upload

--- giftbike.js
+++ giftbike.js
@@ -1,4 +1,4 @@
-// Version 6
+// Version 6.1
 if (typeof Ecwid !== 'undefined') {
   // Функция для установки и проверки cookie
   function setCookie(name, value, days) {
@@ -40,7 +40,7 @@ if (typeof Ecwid !== 'undefined') {
   async function getStoreLanguage() {
     try {
       // Замените <YOUR_PUBLIC_TOKEN> на ваш публичный токен
-      const response = await fetch('https://app.ecwid.com/api/v3/110610642/profile?token=public_<YOUR_PUBLIC_TOKEN>', {
+      const response = await fetch('https://app.ecwid.com/api/v3/110610642/profile?token=public_39m9JHG2hGvffSriWnuL2ajrcGmhL4wg', {
         headers: {
           'Authorization': 'Bearer custom-app-110610642-1'
         }
@@ -106,7 +106,7 @@ if (typeof Ecwid !== 'undefined') {
       <p>Store Language: ${storeLang}</p>
       <p>Country (by IP): ${country}</p>
       <p>Visit: ${isFirstVisit ? 'First Visit' : 'Returning Visit'}</p>
-      <p>Version: 6</p>
+      <p>Version: 6.1</p>
       <div style="margin-top: 10px;">
         <button onclick="changeStoreLanguage('en')">English</button>
         <button onclick="changeStoreLanguage('ru')">Русский</button>
